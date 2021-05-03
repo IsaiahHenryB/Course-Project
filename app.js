cdd5b77619c10674975f6e855101b698faee7233
IsaiahHenry <isaiahhenryb@gmail.com>
2021-05-03 16:31:15 -0400

    experiment branch from HW11

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.use(morgan('combined'))
 
 // Adding app.get
 app.get('/', (request, response) =>{
-    response.render('pages/index');
+    response.render('pages/index', {
+        books: book,
+    });
+
 })
 app.get('/about', (request, response) =>{
     response.render('pages/about');
@@ -35,10 +38,53 @@ app.get('/admin-console/create-book/:id', (request, response) =>{
     response.render('pages/update');
 })
 app.get('/book/:id', (request, response) =>{
-    response.render('pages/book');
+    let params = request.params;
+    console.log(params);
+    if (params.id === '01'){
+        response.render('pages/book',{
+            thisbook: book[0]
+        },)
+    }
+    else if (params.id === '02'){
+        response.render('pages/book',{
+            thisbook: book[1]
+        },)
+    }
+    else if (params.id === '03'){
+        response.render('pages/book',{
+            thisbook: book[2]
+        },)
+    }
+    else {
+        response.send(`This product doesn't exist. Try searching again. `);        
+    };
 })
 
+// Create Book array
 
+let book = [
+    {
+        num: '01',
+        name: 'Fun Home: A Family Tragicomic',
+        author: 'by Alison Bechdel',
+        rating: '5 Stars',
+        img: 'img/fun-home.jpg',
+    },
+    {
+        num: '02',
+        name: 'Watchmen',
+        author: 'by Alan Moore',
+        rating: '5 Stars',
+        img: 'img/Watchmen.jpg',
+    },
+    {
+        num: '03',
+        name: 'Hunter X Hunter Vol.1',
+        author: 'by Yoshihiro Tagashi',
+        rating: '5 Stars',
+        img: 'img/hunter-x-hunter.jpg',
+    },
+]
 // Adding app.listen
 app.listen(PORT, ()=>{
     console.log(`You are currently running from port${PORT}`)
